95d1cefb572b11c450860a3ec6e5e6223047e29d
Andrés Weitzel <andres96energy@hotmail.com>
2023-04-15 00:16:43 -0300

    Updated dynamodb helpers

---
+++ src/controllers/bioetanolPrecios/getByIdBioetanolPrecios.js
@@ -0,0 +1,80 @@
+//Enums
+const {
+    statusCode
+} = require("../../enums/http/statusCode");
+//Helpers
+const {
+    bodyResponse
+} = require("../../helpers/http/bodyResponse");
+const {
+    validateHeadersParams,
+} = require("../../helpers/validator/http/requestHeadersParams");
+const {
+    validateAuthHeaders
+} = require("../../helpers/auth/headers");
+const {
+    getOneItem
+} = require("../../helpers/dynamodb/getOne");
+
+//Const/Vars
+let eventBody;
+let eventHeaders;
+let validateReqParams;
+let validateAuth;
+let obj;
+const BIOET_PRECIOS_TABLE_NAME = process.env.BIOET_PRECIOS_TABLE_NAME;
+
+/**
+ * @description Function to obtain all the objects of the bioethanol prices table according to id
+ * @param {Object} event Object type
+ * @returns a body response with http code and message
+ */
+module.exports.handler = async (event) => {
+    try {
+        //Init
+        obj = null;
+        let body = null;
+
+        //-- start with validation Headers  ---
+        eventHeaders = await event.headers;
+
+        validateReqParams = await validateHeadersParams(eventHeaders);
+
+        if (!validateReqParams) {
+            return await bodyResponse(
+                statusCode.BAD_REQUEST,
+                "Bad request, check missing or malformed headers"
+            );
+        }
+
+        validateAuth = await validateAuthHeaders(eventHeaders);
+
+        if (!validateAuth) {
+            return await bodyResponse(
+                statusCode.UNAUTHORIZED,
+                "Not authenticated, check x_api_key and Authorization"
+            );
+        }
+        //-- end with validation Headers  ---
+
+        //-- start with dynamodb operations  ---
+        
+        //let items = await getOneItem(BIOET_PRECIOS_TABLE_NAME);
+        
+        //-- end with dynamodb operations  ---
+
+
+        return await bodyResponse(
+            statusCode.OK,
+            items
+        );
+
+    } catch (error) {
+        console.log(error);
+        return await bodyResponse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            "An unexpected error has occurred. Try again"
+        );
+    }
+
+}
