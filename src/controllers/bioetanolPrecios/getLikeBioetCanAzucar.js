3284a627c561b8e79a024fccdf000ea1915b17ca
Andrés Weitzel <andres96energy@hotmail.com>
2023-06-06 21:29:40 -0300

    Last checked getLikeBioetMaiz

--- src/controllers/bioetanolPrecios/getLikeBioetCanAzucar.js
+++ src/controllers/bioetanolPrecios/getLikeBioetCanAzucar.js
@@ -1,37 +1,29 @@
 //Enums
-const {
-    statusCode
-} = require("../../enums/http/statusCode");
+const { statusCode } = require("../../enums/http/statusCode");
+const { value } = require("../../enums/general/values");
 //Helpers
+const { bodyResponse } = require("../../helpers/http/bodyResponse");
 const {
-    bodyResponse
-} = require("../../helpers/http/bodyResponse");
+  validateHeadersAndKeys,
+} = require("../../helpers/validations/headers/validateHeadersAndKeys");
 const {
-    validateHeadersParams,
-} = require("../../helpers/validator/http/requestHeadersParams");
+  getAllItemsWithFilter,
+} = require("../../helpers/dynamodb/operations/getAllDynamoDB");
 const {
-    validateAuthHeaders
-} = require("../../helpers/auth/headers");
-const {
-    validatePathParameters,
+  validatePathParameters,
 } = require("../../helpers/http/queryStringParams");
-const {
-    getAllItemsWithFilter
-} = require("../../helpers/dynamodb/operations/getAllWithFilter");
 
 //Const/Vars
-let eventBody;
+const BIOET_PRECIOS_TABLE_NAME = process.env.BIOET_PRECIOS_TABLE_NAME;
+const BIOET_PRECIOS_KEY_DYNAMO = "bioetCanAzucar";
 let eventHeaders;
-let validateReqParams;
-let validateAuth;
+let checkEventHeadersAndKeys;
 let validatePathParam;
-let key;
-let id;
-let pageNro;
 let orderAt;
 let items;
 let bioetCanAzucar;
-const BIOET_PRECIOS_TABLE_NAME = process.env.BIOET_PRECIOS_TABLE_NAME;
+let msg;
+let code;
 
 /**
  * @description Function to obtain all the objects of the bioethanol prices table according to the bioethanol caña azucar prices
@@ -39,84 +31,68 @@ const BIOET_PRECIOS_TABLE_NAME = process.env.BIOET_PRECIOS_TABLE_NAME;
  * @returns a body response with http code and message
  */
 module.exports.handler = async (event) => {
-    try {
-        //Init
-        obj = null;
-        id = '';
-        items = null;
-        pageSizeNro = 5;
-        orderAt = "asc";
+  try {
+    //Init
+    items = value.IS_NULL;
+    pageSizeNro = 5;
+    orderAt = "asc";
 
-        //-- start with validation Headers  ---
-        eventHeaders = await event.headers;
+    //-- start with validation headers and keys  ---
+    eventHeaders = await event.headers;
 
-        validateReqParams = await validateHeadersParams(eventHeaders);
+    checkEventHeadersAndKeys = await validateHeadersAndKeys(eventHeaders);
 
-        if (!validateReqParams) {
-            return await bodyResponse(
-                statusCode.BAD_REQUEST,
-                "Bad request, check missing or malformed headers"
-            );
-        }
-
-        validateAuth = await validateAuthHeaders(eventHeaders);
-
-        if (!validateAuth) {
-            return await bodyResponse(
-                statusCode.UNAUTHORIZED,
-                "Not authenticated, check x_api_key and Authorization"
-            );
-        }
-        //-- end with validation Headers  ---
-
-        //-- start with pagination  ---
-        queryStrParams = event.queryStringParameters;
-
-        if (queryStrParams != null) {
-            pageSizeNro = parseInt(await event.queryStringParameters.limit);
-            orderAt = await event.queryStringParameters.orderAt;
-        }
-        //-- end with pagination  ---
-
-        //-- start with path parameters  ---
-        bioetCanAzucar = await event.pathParameters.bioetCanAzucar;
-
-        validatePathParam = await validatePathParameters(bioetCanAzucar);
-
-        if (!validatePathParam) {
-            return await bodyResponse(
-                statusCode.BAD_REQUEST,
-                "Bad request, check malformed bioetCanAzucar value"
-            );
-        }
-        //-- end with path parameters  ---
-
-
-        //-- start with dynamodb operations  ---
-
-        items = await getAllItemsWithFilter(BIOET_PRECIOS_TABLE_NAME, 'bioetCanAzucar', bioetCanAzucar, pageSizeNro, orderAt);
-
-        if (items == null || !(items.length)) {
-            return await bodyResponse(
-                statusCode.BAD_REQUEST,
-                "The objects with the bioetCanAzucar value is not found in the database"
-            );
-        }
-        //-- end with dynamodb operations  ---
-
-
-        return await bodyResponse(
-            statusCode.OK,
-            items
-        );
-
-    } catch (error) {
-        console.log(`Error in getLikeBioetCanAzucar lambda, caused by ${{error}}`);
-        console.error(error.stack);
-        return await bodyResponse(
-            statusCode.INTERNAL_SERVER_ERROR,
-            "An unexpected error has occurred. Try again"
-        );
+    if (checkEventHeadersAndKeys != value.IS_NULL) {
+      return checkEventHeadersAndKeys;
     }
+    //-- end with validation headers and keys  ---
 
-}
+    //-- start with pagination  ---
+    queryStrParams = event.queryStringParameters;
+
+    if (queryStrParams != value.IS_NULL) {
+      pageSizeNro = parseInt(await event.queryStringParameters.limit);
+      orderAt = await event.queryStringParameters.orderAt;
+    }
+    //-- end with pagination  ---
+
+    //-- start with path parameters  ---
+    bioetCanAzucar = await event.pathParameters.bioetCanAzucar;
+
+    validatePathParam = await validatePathParameters(bioetCanAzucar);
+
+    if (!validatePathParam) {
+      return await bodyResponse(
+        statusCode.BAD_REQUEST,
+        "Bad request, check malformed bioetCanAzucar value"
+      );
+    }
+    //-- end with path parameters  ---
+
+    //-- start with dynamodb operations  ---
+
+    items = await getAllItemsWithFilter(
+      BIOET_PRECIOS_TABLE_NAME,
+      BIOET_PRECIOS_KEY_DYNAMO,
+      bioetCanAzucar,
+      pageSizeNro,
+      orderAt
+    );
+
+    if (items == value.IS_NULL || items == value.IS_UNDEFINED) {
+      return await bodyResponse(
+        statusCode.BAD_REQUEST,
+        "The objects with the bioetCanAzucar value is not found in the database"
+      );
+    }
+    //-- end with dynamodb operations  ---
+
+    return await bodyResponse(statusCode.OK, items);
+  } catch (error) {
+    code = statusCode.INTERNAL_SERVER_ERROR;
+    msg = `Error in GET LIKE BIOET CAN AZUCAR lambda. Caused by ${error}`;
+    console.error(`${msg}. Stack error type : ${error.stack}`);
+
+    return await bodyResponse(code, msg);
+  }
+};
